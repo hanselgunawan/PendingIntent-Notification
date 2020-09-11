fb0e018681f15b6a5f08b7e72074f969bcd0f341
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2020-09-11 03:25:44 -0700

    added action button and notification receiver

--- app/src/main/java/com/hanseltritama/pendingintentnotification/NotificationHelper.kt
+++ app/src/main/java/com/hanseltritama/pendingintentnotification/NotificationHelper.kt
@@ -4,9 +4,11 @@ import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.ContextWrapper
 import android.content.Intent
+import android.graphics.Color
 import androidx.core.app.NotificationCompat
 
 class NotificationHelper(base: Context) : ContextWrapper(base) {
@@ -51,14 +53,31 @@ class NotificationHelper(base: Context) : ContextWrapper(base) {
     fun getChannel1Notification(title: String, message: String) : NotificationCompat.Builder {
 
         val resultIntent = Intent(this, MainActivity::class.java)
-        val resultPendingIntent = PendingIntent.getActivity(this, 1, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+        val resultPendingIntent = PendingIntent.getActivity(this,
+            1,
+            resultIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val broadcastIntent = Intent(this, NotificationReceiver::class.java)
+        broadcastIntent.putExtra("toastMessage", message)
+
+        val actionIntent = PendingIntent.getBroadcast(this,
+            0,
+            broadcastIntent,
+            // to update toastMessage extra's value
+            PendingIntent.FLAG_UPDATE_CURRENT)
 
         return NotificationCompat.Builder(applicationContext,  channel1ID)
             .setContentTitle(title)
             .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+            .setColor(Color.BLUE)
             .setSmallIcon(R.drawable.ic_one)
             .setContentIntent(resultPendingIntent)
+            .setOnlyAlertOnce(true)
             .setAutoCancel(true) // notification will disappear when tapped
+            .addAction(R.mipmap.ic_launcher, "Toast", actionIntent) // can be added up to 3
     }
 
     fun getChannel2Notification(title: String, message: String) : NotificationCompat.Builder {
