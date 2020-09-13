6b78eb44df385429a4031d84ff07e459366052d2
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2020-09-13 05:12:29 -0700

    added progress bar notification

--- app/src/main/java/com/hanseltritama/pendingintentnotification/MainActivity.kt
+++ app/src/main/java/com/hanseltritama/pendingintentnotification/MainActivity.kt
@@ -2,7 +2,9 @@ package com.hanseltritama.pendingintentnotification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         btnChannel2.setOnClickListener {
-            sendOnChannel2(title_field.text.toString(), message_field.text.toString())
+            sendOnChannel2()
         }
     }
 
@@ -28,8 +30,34 @@ class MainActivity : AppCompatActivity() {
         notificationHelper.sendOnChannel1()
     }
 
-    private fun sendOnChannel2(title: String, message: String) {
-        val notificationBuilder: NotificationCompat.Builder = notificationHelper.getChannel2Notification(title, message)
-        notificationHelper.getManager().notify(2, notificationBuilder.build())
+    private fun sendOnChannel2() {
+
+        val progressMax = 100
+
+        val notification: NotificationCompat.Builder = NotificationCompat.Builder(this,  "channel2ID")
+            .setSmallIcon(R.drawable.ic_two)
+            .setContentTitle("Download")
+            .setContentText("Download in progress")
+            .setOnlyAlertOnce(true)
+            .setOngoing(true)
+            .setPriority(NotificationCompat.PRIORITY_LOW)
+            .setProgress(progressMax, 0, false)
+            .setAutoCancel(true)
+
+        val notificationManager = NotificationManagerCompat.from(this)
+        notificationManager.notify(2, notification.build())
+
+        Thread(Runnable {
+            SystemClock.sleep(2000)
+            for (progress in 0..progressMax step 10) {
+                notification.setProgress(progressMax, progress, false)
+                notificationManager.notify(2, notification.build())
+                SystemClock.sleep(1000)
+            }
+            notification.setContentText("Download finished")
+                .setProgress(0,0,false)
+                .setOngoing(false)
+            notificationManager.notify(2, notification.build())
+        }).start()
     }
 }
